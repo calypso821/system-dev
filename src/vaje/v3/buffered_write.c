1245960f050f779ab0a8cb4eb2d87101e98b8508
Matic Knez <matic.knez82@gmail.com>
2024-11-06 09:38:04 +0100

    Add vaja 3: buffered_write, kamned

--- src/vaje/v3/buffered_write.c
+++ src/vaje/v3/buffered_write.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <err.h>
+#include <time.h>
 
 
 #define BUFSIZE1 10
@@ -7,34 +8,76 @@
 
 int main(int argc, char *argv)
 {
-    char *buf1[BUFSIZE1];
-    char *buf2[BUFSIZE2];
+    // time_t start, end; // Second percision only
+    struct timespec start, end;
+    double elapsed;
+
+    char buf[BUFSIZE2];
 
     FILE *fp1;
     FILE *fp2;
 
     // Create both files (streams with file pointer)
     if ((fp1 = fopen("out1.txt", "w")) == NULL) {
-        err("failed to open out1.txt");
+        err(1, "failed to open out1.txt");
     }
     if ((fp2 = fopen("out2.txt", "w")) == NULL) {
-        err("failed to open out2.txt");
+        err(1, "failed to open out2.txt");
     }
 
-    // Set buffers to full buffering mode
-    if (setvbuf(fp1, buf1, _IOFBF, BUFSIZE1)) {
-        err("failed to set bufffer 1");
+    char *line = "Danes je lep dan!\n"; // 19 chars (2x calls)
+    //char *line = "Danes je\n"; // 10 chars (1x call)
+
+    // Set buffer to full buffering mode and size of 10 and 100
+    if (setvbuf(fp1, buf, _IOFBF, BUFSIZE1)) {
+        err(1, "failed to set bufffer");
     }
-    if (setvbuf(fp2, buf2, _IOFBF, BUFSIZE2)) {
-        err("failed to set buffer 2");
+    if (setvbuf(fp2, buf, _IOFBF, BUFSIZE2)) {
+        err(1, "failed to set buffer");
     }
 
-    char *line = "Danes je lep dan!";
+    // Get current time (NULL)
+    clock_gettime(CLOCK_REALTIME, &start);
 
-    // Izpisi 1000000 v out1 in out2
-    time(NULL)
+    for (int i = 0; i < 5000000; i++)
+    {
+        if (fputs(line, fp1) == EOF) {
+            err(1, "fputs filed");
+        }
+    }
+
+    clock_gettime(CLOCK_REALTIME, &end);
+    elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) * 1e-9;
+    printf("Elapsed time fully buffered stream (buffer size: %d): %fs\n", BUFSIZE1, elapsed);
+
+
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    for (int i = 0; i < 5000000; i++)
+    {
+        if (fputs(line, fp2) == EOF) {
+            err(1, "fputs filed");
+        }
+    }
+
+    clock_gettime(CLOCK_REALTIME, &end);
+    elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) * 1e-9;
+    printf("Elapsed time fully buffered stream (buffer size: %d): %fs\n", BUFSIZE1, elapsed);
+
+    // line length = 20
+    // Buffer 1 (size 10) - 2x system calls per fputs()
+    // 5x fputs() ... 10x syscalls
+    // Buffer 2 (size 100) - 1x system call per 5 fputs()
+    // 5x fputs() ... 1x syscall
 
 
 
+    if (fclose(fp1) == EOF) {
+        err(1, "faild closing stream");
+    }
+    if (fclose(fp2) == EOF) {
+        err(1, "faild closing stream");
+    }
 
+    return 0;
 }
---
+++ src/vaje/v3/kamned.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+
+#define BUFSIZE 254
+
+int main (int argc, char *argv)
+{
+    char *mode;
+    int n;
+
+    if (argc == 3 && strcmp(argv[2], "-a") == 0) {
+        // Append mode
+        mode = "a";
+    }
+    else if (argc == 4 && strcmp(argv[2], "-d") == 0) {
+        // Delete mode
+        n = atoi(argv[3]);
+        mode = "r+";
+    }
+    else if (argc == 4 && strcmp(argv[2], "-i") == 0) {
+        // Insert mode
+        n = atoi(argv[3]);
+        mode = "r+";
+    }
+    else {
+        fprintf(stderr, "Invalid arguments\n");
+        fprintf(stderr, "Usage: %s <filename> [-a | -d <n> | -i <n>]\n", argv[0]);
+        return 1;
+    }
+
+    FILE *fp;
+    char buf[BUFSIZE];
+    char *line;
+
+    if ((fp = fopen(argv[1], mode)) == NULL) {
+        err(1, "failed opening file %s", argv[1]);
+    }
+
+    
+    if (strcmp(argv[2], "-a"))
+    {
+        // Append mode
+
+        // Read from stdin -> append to end of file
+        // Stop Ctrl + D (EOF)
+        // Line buffereed (stream propery) of stdin
+        while (fgets(buf, sizeof(buf), stdin) != NULL)
+        {
+            // Full buffereed (stream propery) of fp
+            if (fputs(buf, fp) == EOF) {
+                err(1, "fputs filed");
+            }
+        }
+        // Check exit status of wile loop (fgets)
+        if (ferror(stdin)) {
+            err(1, "fgets failed");
+        }
+        // If Ctrl + D pressed = EOF (no error)
+    }
+    else if (strcmp(argv[2], "-d"))
+    {
+        // Delete mode
+        FILE *temp;
+        if ((temp = fopen("temp.txt", "w")) == NULL) {
+            err(1, "failed opening temp file");
+        }
+        int cnt = 0;
+
+        // 1. Write all lines from original file to temp (execpt n line)
+        while (fgets(buf, sizeof(buf), fp) != NULL)
+        {
+            if (cnt != n) {
+                if (fputs(buf, temp) == EOF) {
+                    err(1, "fputs filed");
+                }
+            }
+            cnt++;
+        }
+        // Check exit status of wile loop (fgets)
+        if (ferror(fp)) {
+            err(1, "fgets failed");
+        }
+
+        // 2. Write back all lines from temp to original file 
+        // Set seek of temp to 0 
+        // truncate fp
+        while (fgets(buf, sizeof(buf), temp) != NULL)
+        {
+            if (fputs(buf, fp) == EOF) {
+                err(1, "fputs filed");
+            }
+        }
+        // Check exit status of wile loop (fgets)
+        if (ferror(temp)) {
+            err(1, "fgets failed");
+        }
+
+    }
+}
